0aeedacf35218f7c98285fd2fb061cf11f39b7ee
Chris Lee <chrisli323@gmail.com>
2025-02-17 03:46:36 -0800

    [Feature] Add settings view

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -44,7 +44,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Swiftful!")
+            Text("Wusssup!")
             
             Button("Subscribe") {
                 
